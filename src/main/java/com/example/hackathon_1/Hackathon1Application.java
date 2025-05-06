048705119a4a4cb04775f984254a5fd5eb0970a9
Joaquin Lopez <159632612+elgatohumilde@users.noreply.github.com>
2025-05-06 16:28:10 -0500

    feat: add spring initializr dependencies

---
+++ src/main/java/com/example/hackathon_1/Hackathon1Application.java
@@ -0,0 +1,13 @@
+package com.example.hackathon_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackathon1Application.class, args);
+	}
+
+}
